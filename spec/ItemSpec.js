19b4bd94272babe4dc5c30ac6b1cd115b90282f2
Jonathan R. Wallace <jonathan.wallace@gmail.com>
2018-02-16 16:29:42 -0500

    Adds other Item specs

--- spec/ItemSpec.js
+++ spec/ItemSpec.js
@@ -7,4 +7,27 @@ describe("Item", function() {
 
     expect(item.external_id).toEqual(1);
   });
+
+  it("should have a name", function() {
+    item = new Item(1, "shovel");
+
+    expect(item.name).toEqual("shovel");
+  });
+
+  it("should have a location", function() {
+    item = new Item(1, "shovel", 3)
+
+    expect(item.location).toEqual(3);
+  });
+  it("should have a score", function() {
+    item = new Item(1, "shovel", 3, 20)
+
+    expect(item.score).toEqual(20);
+  });
+
+  it("should have text", function() {
+    item = new Item(1, "shovel", 3, 20, "shovel description")
+
+    expect(item.text).toEqual("shovel description");
+  });
 });
