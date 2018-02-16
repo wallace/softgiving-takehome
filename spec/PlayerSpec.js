fc488b772a519feef7d20a690c4548a635fc2dd4
Jonathan R. Wallace <jonathan.wallace@gmail.com>
2018-02-16 16:55:44 -0500

    Ensures Player.score() adds correctly

--- spec/PlayerSpec.js
+++ spec/PlayerSpec.js
@@ -14,9 +14,18 @@ describe("Player", function() {
     expect(player.inventory).toEqual([]);
   });
 
-  it("should have a score", function() {
+  it("should have a default score", function() {
     player = new Player(1);
 
     expect(player.score()).toEqual(0);
+  });
+
+  it("should have a score", function() {
+    player = new Player(1);
+
+    player.add_item({ score: 1 });
+    player.add_item({ score: 2 });
+
+    expect(player.score()).toEqual(3);
   });
 });
