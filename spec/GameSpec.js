8d1edda3aa9a65f840a966ac180672a7efb56d9e
Jonathan R. Wallace <jonathan.wallace@gmail.com>
2018-02-17 07:08:52 -0500

    Updates Game to know the current_room
    Updates to the more up to date syntax

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -3,14 +3,20 @@ describe("Game", function() {
   var game;
 
   it("should have no rooms", function() {
-    game = new Game();
+    game = new Game([], "player");
 
     expect(game.rooms).toEqual([]);
   });
 
   it("should have no player", function() {
-    game = new Game();
+    game = new Game("room", "player");
 
-    expect(game.player).toEqual(null);
+    expect(game.player).toEqual("player");
+  });
+
+  it("should know the current room of the player", function() {
+    game = new Game([], null, 1);
+
+    expect(game.current_room).toEqual(1);
   });
 });
