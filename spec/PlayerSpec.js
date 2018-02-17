eccdd644f6173bc5e76ba5e2b633f4a5254b9507
Jonathan R. Wallace <jonathan.wallace@gmail.com>
2018-02-17 07:05:34 -0500

    Remove current_room from player
    This is game state and will be managed by the game.

--- spec/PlayerSpec.js
+++ spec/PlayerSpec.js
@@ -2,12 +2,6 @@ describe("Player", function() {
   var Player = require('../lib/Player');
   var player;
 
-  it("should have a current_room", function() {
-    player = new Player(1);
-
-    expect(player.current_room).toEqual(1);
-  });
-
   it("should have no inventory", function() {
     player = new Player(1);
 
